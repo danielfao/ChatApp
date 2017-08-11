809166cb0bb6681ff6b5aab371cb118263f89ddc
Daniel Oliveira <daniel.fao@gmail.com>
2017-08-11 09:58:54 -0300

    Implemented Users list

---
+++ ChatApp/MessagesViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Daniel Oliveira on 8/7/17.
+//  Copyright © 2017 Daniel Oliveira. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class MessagesViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+        
+        let image = UIImage(named: "write_message")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(handleNewMessage))
+        
+        checkIfUserIsLoggedIn()
+    }
+    
+    func handleNewMessage() {
+        let newMessageController = NewMessageTableViewController()
+        let navController = UINavigationController(rootViewController: newMessageController)
+        present(navController, animated: true, completion: nil)
+    }
+    
+    func checkIfUserIsLoggedIn() {
+        //user is not logged in
+        if Auth.auth().currentUser?.uid == nil {
+            perform(#selector(handleLogout), with: nil, afterDelay: 0)
+            handleLogout()
+        } else {
+            let uid = Auth.auth().currentUser?.uid
+            Database.database().reference().child("users").child(uid!).observe(.value, with: { (snapshot) in
+                if let dictionary = snapshot.value as? [String: AnyObject] {
+                    self.navigationItem.title = dictionary["name"] as? String
+                }
+            })
+        }
+    }
+    
+    func handleLogout() {
+        do {
+            try Auth.auth().signOut()
+        } catch let logoutError {
+            print(logoutError)
+        }
+        
+        let loginController = LoginViewController()
+        present(loginController, animated: true, completion: nil)
+    }
+}
---
+++ ChatApp/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  ChatApp
+//
+//  Created by Daniel Oliveira on 8/11/17.
+//  Copyright © 2017 Daniel Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    var name: String?
+    var email: String?
+}
